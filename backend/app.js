522cadc77bfe80a1a846fa2255933888d70f7004
Daze <44749931+Errleng@users.noreply.github.com>
2021-10-17 10:58:43 -0400

    Enable CORS

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const db = require('./mongo');
 const config = require('./config');
 
@@ -18,6 +19,7 @@ const app = express();
 
 const apiRouter = require('./apiRouter');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
